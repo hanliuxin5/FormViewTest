2e83a9ad5586b09753e4ecdbc7c1c295fe4a35eb
hanliuxin <hanliuxin123@Hotmail.com>
2016-09-01 19:10:08 +0800

    表单基本完成

--- app/src/main/java/com/lychee/formviewtest/MainActivity.java
+++ app/src/main/java/com/lychee/formviewtest/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.lychee.formviewtest.lychee.APIContext;
 import com.lychee.formviewtest.lychee.FuckJson;
@@ -20,9 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     FrameLayout fl;
 
-    float k1;
-    float k2;
-    //理论范围
+    //理论布局范围
     float maxX;
     float maxY;
     //屏幕宽高
@@ -31,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
     //缩放系数
     float scalyX;
     float scalyY;
+    int supplementX;
+    //初始显示人员viewGroup
+    int w_width;
+    int w_height;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +47,10 @@ public class MainActivity extends AppCompatActivity {
         //获取屏幕数据
         getScreenInfo();
         //获取数据
+        //fa315fce76c61a319601014f1d601a79 简短
         //9db5fe0082936fa914e5d41cd14e7ddc 简单
         //6ba4a4debe744bcc695559472196c259 复杂
+        //ea97edfec1a47f1d27cfc8a68280a423 很复杂混双
         APIContext.findKnockOutScore4OldCells("6ba4a4debe744bcc695559472196c259", new MyOkHttpCallback(this) {
             @Override
             public void onSuccess(String json) {
@@ -97,65 +106,87 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void analyzeDate(OldCellBean bean) {
+
         seekMaxX(bean);
         seekMaxY(bean);
+        OldCellBean.ResultEntity.SeatsEntity.XlineEntity x0 = bean.getResult().getSeats().getXline().get(0);
+        OldCellBean.ResultEntity.SeatsEntity.XlineEntity x1 = bean.getResult().getSeats().getXline().get(1);
 
-        k1 = maxX / width;
-        k2 = maxY / height;
+//        supplementX = Integer.parseInt(x0.getX1());
+//        float k1 = (maxX - supplementX) / width;
+        float k1 = maxX / width;
+        float k2 = maxY / height;
 
 
         scalyX = k1;
         scalyY = k1;
+//        scalyY = (float) (k1 / 1.5);
+
         LogUtil.d("scalyX=" + scalyX + ",scalyY" + scalyY);
 
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams((int) width, (int) (maxY / scalyY) + 40);
-        params.gravity = Gravity.CENTER;
-//        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) fl.getLayoutParams();
-//        params.width = (int) width;
-//        params.height = params.height + 100;
-        fl.setBackgroundColor(Color.WHITE);
-        fl.setLayoutParams(params);
-//        fl.requestLayout();
+
+        w_width = (int) (Integer.parseInt(x0.getX1()) / scalyX);
+        w_height = (int) ((Integer.parseInt(x1.getY1()) - Integer.parseInt(x0.getY1())) / 2 / scalyY);
+
+        LogUtil.d("修正前：w_width=" + w_width + ",w_height=" + w_height);
+
+        if (w_width > 400 || w_width < 300) {
+            supplementX = w_width - 400;
+            w_width = 400;
+
+//            (int) (maxY / scalyY) + 29
+//            (int) (width + Math.abs(supplementX))
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            fl.setLayoutParams(params);
+
+        }
 
         drawX(bean.getResult().getSeats().getXline());
-//        drawY(bean.getResult().getSeats().getYline());
+        drawY(bean.getResult().getSeats().getYline());
+
+
+        drawLootBooks(bean.getResult().getLotbook(), bean.getResult().getSeats().getXline());
+        LogUtil.d("修正后：w_width=" + w_width + ",w_height=" + w_height + ",supplementX=" + supplementX);
 
 
     }
 
     private void drawX(List<OldCellBean.ResultEntity.SeatsEntity.XlineEntity> list) {
-        View X;
+        ImageView X;
+        FrameLayout.LayoutParams params;
         OldCellBean.ResultEntity.SeatsEntity.XlineEntity item;
+
         for (int i = 0; i < list.size(); i++) {
+
             item = list.get(i);
             int width = Math.abs((int) ((Integer.parseInt(item.getX2()) - Integer.parseInt(item.getX1())) / scalyX));
-            int height = 2;
+            int height = ScreenUtils.pxToDpCeilInt(this, 2);
             int left = 0;
             if (item.getSortNo().split("-").length == 3) {
                 left = (int) (Integer.parseInt(item.getX1()) / scalyX);
             } else if (item.getSortNo().split("-").length == 4) {
                 left = (int) (Integer.parseInt(item.getX2()) / scalyX);
             }
-            int top = (int) (Integer.parseInt(item.getY1()) / scalyX);
+            int top = (int) (Integer.parseInt(item.getY1()) / scalyY);
 
-            X = new View(this);
-            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width, height);
-            params.leftMargin = left;
+            X = new ImageView(this);
+            params = new FrameLayout.LayoutParams(width, height);
+            params.leftMargin = left - supplementX;
+//            params.leftMargin = left;
             params.topMargin = top;
 
             if ("1".equals(item.getExtra())) {
-//                if (item.getSortNo().split("-").length == 3) {
-//                    X.setBackgroundColor(getResources().getColor(R.color.blue));
-//                } else if (item.getSortNo().split("-").length == 4) {
-//                    X.setBackgroundColor(getResources().getColor(R.color.black));
-//                }
+
+                X.setBackgroundDrawable(getResources().getDrawable(R.drawable.dash_line));
+                X.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
             } else {
-                X.setBackgroundColor(getResources().getColor(R.color.green));
-                X.setLayoutParams(params);
-                fl.addView(X);
+
+                X.setBackgroundColor(Color.GREEN);
+
             }
-//            X.setLayoutParams(params);
-//            fl.addView(X);
+            X.setLayoutParams(params);
+            fl.addView(X);
         }
 
 
@@ -166,19 +197,101 @@ public class MainActivity extends AppCompatActivity {
         OldCellBean.ResultEntity.SeatsEntity.YlineEntity item;
         for (int i = 0; i < list.size(); i++) {
             item = list.get(i);
-            int width = 2;
-            int height = (int) ((Integer.parseInt(item.getY2()) - Integer.parseInt(item.getY1())) / scalyX);
+            int width = ScreenUtils.pxToDpCeilInt(this, 2);
+            int height = (int) ((Integer.parseInt(item.getY2()) - Integer.parseInt(item.getY1())) / scalyY);
             int left = (int) (Integer.parseInt(item.getX1()) / scalyX);
-            int top = (int) (Integer.parseInt(item.getY1()) / scalyX);
+            int top = (int) (Integer.parseInt(item.getY1()) / scalyY);
 
             Y = new View(this);
             FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width, height);
-            params.leftMargin = left;
+            params.leftMargin = left - supplementX;
+//            params.leftMargin = left;
             params.topMargin = top;
             Y.setBackgroundColor(Color.RED);
             Y.setLayoutParams(params);
             fl.addView(Y);
         }
     }
+
+    private void drawLootBooks(List<OldCellBean.ResultEntity.LotbookEntity> list, List<OldCellBean.ResultEntity.SeatsEntity.XlineEntity> xList) {
+        FrameLayout.LayoutParams fParams;
+        LinearLayout whole;
+
+        LinearLayout.LayoutParams tParams;
+        TextView tv_order;
+        TextView tv_number;
+
+
+        LinearLayout.LayoutParams lParams;
+        LinearLayout people;
+
+        LinearLayout.LayoutParams aParams;
+        ImageView avatar;
+        LinearLayout.LayoutParams nParams;
+        TextView name;
+
+        OldCellBean.ResultEntity.SeatsEntity.XlineEntity x0;
+        OldCellBean.ResultEntity.LotbookEntity lot;
+        int p_x = 0;
+        int p_y;
+
+        for (int i = 0; i < list.size(); i++) {
+            x0 = xList.get(i);
+            lot = list.get(i);
+
+            fParams = new FrameLayout.LayoutParams(w_width, w_height);
+            fParams.leftMargin = p_x;
+            p_y = (int) (Integer.parseInt(x0.getY1()) / scalyY - w_height / 2);
+            fParams.topMargin = p_y;
+
+
+            whole = new LinearLayout(this);
+            whole.setLayoutParams(fParams);
+            whole.setOrientation(LinearLayout.HORIZONTAL);
+
+            tParams = new LinearLayout.LayoutParams(0, w_height, 1.0f);
+            tv_order = new TextView(this);
+            tv_number = new TextView(this);
+            tv_order.setLayoutParams(tParams);
+            tv_number.setLayoutParams(tParams);
+            tv_order.setGravity(Gravity.CENTER);
+            tv_number.setGravity(Gravity.CENTER);
+            tv_order.setTextSize(8);
+            tv_number.setTextSize(8);
+            tv_order.setText(lot.getSortNo());
+            tv_number.setText(lot.getCodeLetter());
+
+            lParams = new LinearLayout.LayoutParams(w_width / 22 * 15, w_height);
+            people = new LinearLayout(this);
+            people.setOrientation(LinearLayout.HORIZONTAL);
+            people.setLayoutParams(lParams);
+            people.setBackgroundDrawable(getResources().getDrawable(R.drawable.bg_frame_red));
+
+
+//            if (!lot.getIcon().contains(";")) {
+//                //单团
+//                avatar = new ImageView(this);
+//                aParams = new LinearLayout.LayoutParams(w_height, w_height);
+//                avatar.setLayoutParams(aParams);
+//                ImageLoader.getInstance().displayImage(UiHelper.getImageUrl(lot.getIcon()), avatar, UiHelper.r360Options());
+//
+//                name = new TextView(this);
+//                nParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, w_height);
+//                name.setLayoutParams(nParams);
+//                name.setText(lot.getName());
+//            } else if (lot.getIcon().split(";").length == 2) {
+//                //双
+//
+//            }
+
+
+            whole.addView(tv_order);
+            whole.addView(tv_number);
+            whole.addView(people);
+
+            fl.addView(whole);
+        }
+    }
+
 
 }
--- app/src/main/java/com/lychee/formviewtest/lychee/UiHelper.java
+++ app/src/main/java/com/lychee/formviewtest/lychee/UiHelper.java
@@ -3,9 +3,13 @@ package com.lychee.formviewtest.lychee;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.lychee.formviewtest.AppContext;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
 
 /**
@@ -45,4 +49,38 @@ public class UiHelper {
         mHandler.sendMessage(mHandler.obtainMessage(0, 0, duration, text));
     }
 
+
+    public static String IMAGE_URL = "http://www.vvsai.com/images/";
+
+    public static String getImageUrl(String url) {
+        if (TextUtils.isEmpty(url)) {
+            return "";
+        }
+        if (url.startsWith("http:")) {
+            return url;
+        } else if (url.startsWith("/") && url.length() > 1) {
+            return IMAGE_URL + url.substring(1);
+        }
+        return IMAGE_URL + url;
+    }
+
+    /**
+     * 手动设置本地矩形图片为圆角
+     *
+     * @return
+     */
+    public static DisplayImageOptions r360Options() {
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                // 设置正在加载图片
+//                .showImageOnLoading(R.drawable.) //1.8.7新增
+//                .showImageForEmptyUri(R.drawable.ic_pit)
+                // 设置加载失败图片
+//                .showImageOnFail(R.drawable.ic_pit)
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .displayer(new RoundedBitmapDisplayer(360)) // 设置图片角度,0为方形，360为圆角
+                .imageScaleType(ImageScaleType.EXACTLY_STRETCHED)// 拉伸图片
+                .build();
+        return options;
+    }
 }
